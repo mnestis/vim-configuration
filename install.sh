91d1e0374728d11a7d8aab8c17515ed2c4c3a7a5
Darren Richardson <noreply@mnestis.net>
2017-04-20 17:08:27 +0100

    Fix some issues with the install script.

--- install.sh
+++ install.sh
@@ -19,6 +19,29 @@ mkdir -p ~/.vim/autoload
 cp -vr vim-pathogen/autoload/* ~/.vim/autoload/
 
 # Install flake8
-pip install flake8
+pip install --user flake8==3.3.0
 mkdir -p ~/.vim/bundle/vim-flake8 
 cp -vr vim-flake8/* ~/.vim/bundle/vim-flake8/
+
+# If not already present, append ~/.local/bin to PATH
+
+BASHRC_FIXED=$(grep "export PATH=.*/.local/bin:" ~/.bashrc | wc -l)
+
+if [[ $BASHRC_FIXED == 0 ]]
+then
+    echo "Installing ~/.local/bin to PATH in ~/.bashrc"
+    echo "export PATH=~/.local/bin:\$PATH" >> ~/.bashrc
+else
+    echo "~/.local/bin already installed to PATH in ~/.bashrc"
+fi
+
+BASHPROF_FIXED=$(grep "export PATH=.*/.local/bin:" ~/.bash_profile | wc -l)
+
+if [[ $BASHPROF_FIXED == 0 ]]
+then
+    echo "Installing ~/.local/bin to PATH in ~/.bash_profile"
+    echo "export PATH=~/.local/bin:\$PATH" >> ~/.bash_profile
+else
+    echo "~/.local/bin already installed to PATH in ~/.bash_profile"
+fi
+
